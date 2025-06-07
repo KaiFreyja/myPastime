b6e84caec37489a993f11e43e47ee0a077da4e0d
kai <kaiyou2013@gmail.com>
2025-06-07 17:17:45 +0800

    改ip

--- unityProduct/Assets/Script/Config.cs
+++ unityProduct/Assets/Script/Config.cs
@@ -7,7 +7,7 @@ public class Config
 #if UNITY_EDITOR
     public static string IP = "http://192.168.0.170";
 #else
-    public static string IP = "http://35.185.132.13";
+    public static string IP = "http://35.185.174.5";
 #endif
     public static string API_DOMAIN
     {
